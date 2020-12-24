3030673b8f9cc8547ee29cb00a9da90af33e26ce
vishal-mourya <52701972+vishal-mourya@users.noreply.github.com>
2020-12-24 23:35:28 +0530

    updated cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 require('./models/Url');
-require('cors');
+cors = require('cors');
 mongoose = require('mongoose');
 
 // mongodb+srv://vishalmourya:@Aa305132#@cluster0.7kfzg.mongodb.net/short-karo?retryWrites=true&w=majority
@@ -27,7 +27,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors);
 // app.use(cors());
 const Url = mongoose.model('Url');
 
